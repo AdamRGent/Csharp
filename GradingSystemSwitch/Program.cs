4de299b2acadc76bd33466dc9c8f39a900136d20
AdamRGent <adrgent09@hotmail.com>
2023-02-03 13:38:38 +0000

    Updated gradingsystemswitch

--- GradingSystemSwitch/Program.cs
+++ GradingSystemSwitch/Program.cs
@@ -7,36 +7,37 @@ class Program
     public static void Main(string[] args)
     {
         try { 
-        Console.WriteLine("Please enter your grade?");
-        int grade = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Please enter your grade?");
+              int grade = Convert.ToInt32(Console.ReadLine());
             
 
-            switch (grade)
-            {
-                case >100:
-                    Console.WriteLine("Maximum input is 100");
-                    break;
+                switch (grade)
+                {
+                    case >100:
+                              Console.WriteLine("Maximum input is 100");
+                              break;
 
-                case >= 80:
-                    Console.WriteLine("You have passed with Distinction");
-                    break;
+                    case >= 80:
+                              Console.WriteLine("You have passed with Distinction");
+                              break;
 
-                case >= 60:
-                    Console.WriteLine("You have passed with Merit");
-                    break;
+                    case >= 60:
+                              Console.WriteLine("You have passed with Merit");
+                              break;
 
-                case >= 50:
-                    Console.WriteLine("You have Passed");
-                    break;
+                    case >= 50:
+                              Console.WriteLine("You have Passed");
+                              break;
 
-                default:
-                    Console.WriteLine("You have Failed");
-                    break;
+                    default:
+                              Console.WriteLine("You have Failed");
+                              break;
+                }
             }
-        }
+
         catch(Exception ex)
-        {
-            Console.WriteLine("You can only enter digits");
-        }
+            {
+                Console.WriteLine("You can only enter digits");
+            }
     }
 }
