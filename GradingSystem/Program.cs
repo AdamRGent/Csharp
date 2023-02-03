b6e7d2cf41d657de3e0ad5feefd0140aca2446fa
AdamRGent <adrgent09@hotmail.com>
2023-02-03 12:59:31 +0000

    added gradingsystem

---
+++ GradingSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your grade?");
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        string x= grade >= 50 ? "pass" : "fail";
+
+        Console.WriteLine(x);
+
+    }
+}
